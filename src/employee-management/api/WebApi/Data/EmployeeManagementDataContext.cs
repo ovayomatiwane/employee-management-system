73b031b0e078874027b22536b2673f4d6bae3a6c
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-13 18:19:50 +0200

    Database context

---
+++ src/employee-management/api/WebApi/Data/EmployeeManagementDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Entities;
+using Task = WebApi.Models.Entities.Task;
+
+namespace WebApi.Data
+{
+    public class EmployeeManagementDataContext : DbContext
+    {
+        public EmployeeManagementDataContext(DbContextOptions<EmployeeManagementDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Consultant> Consultants { get; set; }
+        public DbSet<ConsultantTask> ConsultantTasks { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
