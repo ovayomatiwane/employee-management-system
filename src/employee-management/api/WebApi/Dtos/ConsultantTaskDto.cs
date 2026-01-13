1590d7644c86c1fde5f77ea9fe343e67780be1ed
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-13 18:22:39 +0200

    Dto's for data to be sent to the frontend

---
+++ src/employee-management/api/WebApi/Dtos/ConsultantTaskDto.cs
@@ -0,0 +1,25 @@
+﻿using WebApi.Models.Entities;
+
+namespace WebApi.Dtos
+{
+    public class ConsultantTaskDto
+    {
+        public required Guid Id { get; set; }
+
+        public Guid RoleId { get; set; }
+
+        public Guid TaskId { get; set; }
+
+        public Guid ConsultantId { get; set; }
+
+        public DateTime? AssignedDate { get; set; }
+
+        public int HoursCompleted { get; set; }
+
+        public ConsultantDto Consultant { get; set; }
+
+        public TaskDto Task { get; set; }
+
+        public RoleDto Role { get; set; }
+    }
+}
---
+++ src/employee-management/api/WebApi/Dtos/TaskDto.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi.Dtos
+{
+    public class TaskDto
+    {
+        public required Guid Id { get; set; }
+
+        public string? TaskName { get; set; }
+
+        public string? Description { get; set; }
+
+        public int MaxDuration { get; set; }
+
+        public DateTime? CreatedDate { get; set; }
+    }
+}
