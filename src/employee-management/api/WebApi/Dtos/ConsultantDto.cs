d870fc8b992a195557d25e942e1db8fbbdab2cd9
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-13 19:59:20 +0200

    Update consultant mapper

--- src/employee-management/api/WebApi/Dtos/ConsultantDto.cs
+++ src/employee-management/api/WebApi/Dtos/ConsultantDto.cs
@@ -3,6 +3,10 @@
 public class ConsultantDto
 {
     public required Guid Id { get; set; }
+
     public required string FirstName { get; set; }
+
     public required string LastName { get; set; }
+
+    public string? EmailAddress { get; set; }
 }
