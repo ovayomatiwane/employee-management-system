69c9ae7e3e0a777899ee1c8167cfd00e9b2010d3
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-14 10:53:57 +0200

    Create and update role rates

--- src/employee-management/api/WebApi/Services/Interfaces/IRoleRatesService.cs
+++ src/employee-management/api/WebApi/Services/Interfaces/IRoleRatesService.cs
@@ -5,6 +5,10 @@ namespace WebApi.Services.Interfaces
 {
     public interface IRoleRatesService
     {
+        Task<RoleRateDto> CreateRoleRates(RoleRateDto roleRate, CancellationToken cancellationToken = default);
+
         Task<IEnumerable<RoleRateDto>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task<RoleRateDto> UpdateRoleRates(RoleRateDto roleRate, CancellationToken cancellationToken = default);
     }
 }
