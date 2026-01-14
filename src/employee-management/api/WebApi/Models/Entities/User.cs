2086aa0f32358c43f00931d9e763eb1eb1fdac33
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-14 09:22:52 +0200

    Add basic auth table

---
+++ src/employee-management/api/WebApi/Models/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace WebApi.Models.Entities
+{
+    public class User
+    {
+        public required Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string Surname { get; set; }
+
+        public required string Email { get; set; }
+
+        public required string PasswordHash { get; set; }
+    }
+}
