e713a1db8273b896da68ad37a0c1333f2efe6bc1
Ovayo Matiwane <ovayo.matiwane@entelect.co.za>
2026-01-14 09:24:30 +0200

    User requests

---
+++ src/employee-management/api/WebApi/Dtos/LoginRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Dtos
+{
+    public class LoginRequestDto
+    {
+        public required string Username { get; set; }
+
+        public required string Password { get; set; }
+    }
+}
---
+++ src/employee-management/api/WebApi/mapping/UserMapping.cs
@@ -0,0 +1,24 @@
+﻿using WebApi.Dtos;
+using WebApi.Models.Entities;
+
+namespace WebApi.mapping
+{
+    public static class UserMapping
+    {
+        public static UserDto MapToDto(this User user)
+        {
+            return new()
+            {
+                Id = user.Id,
+                Name = user.Name,
+                Surname = user.Surname,
+                Email = user.Email,
+            };
+        }
+
+        public static IEnumerable<UserDto> MapToDtos(this IEnumerable<User> users)
+        {
+            return users.Select(x => x.MapToDto());
+        }
+    }
+}
